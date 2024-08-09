0afc638a1c9668759314710754bfc630fd30463f
simage <simage@gmail.com>
2024-08-09 05:04:13 +0000

    modernize (as much as possible) the build toolchain

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,52 +1,60 @@
 const resolve = require('path').resolve;
 
 const CopyPlugin = require('copy-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const I18nPlugin = require('i18n-webpack-plugin');
+const ExtractCSSPlugin = require('mini-css-extract-plugin');
+const LocalizePlugin = require('webpack-localize-assets-plugin');
 
 const BUILD_DIR = resolve(__dirname, 'dist');
 
-const copyAssets = new CopyPlugin([{ from: './assets', to: 'assets', ignore: '*.svg' }]);
-const copySite = new CopyPlugin([{ from: './example', to: './' }]);
-
-const extractSass = new ExtractTextPlugin({ filename: 'leaflet-measure.css' });
-
-const htmlLoader = {
-  test: /\.html$/,
-  use: { loader: 'html-loader?interpolate' }
-};
-
-const scssLoader = {
-  test: /\.scss$/,
-  use: extractSass.extract({
-    use: [
-      {
-        loader: 'css-loader',
-        options: { sourceMap: true, url: false }
+const copyAssets = new CopyPlugin({
+  patterns: [
+    {
+      from: './assets',
+      to: 'assets',
+      globOptions: {
+        ignore: ['*.svg'],
       },
-      {
-        loader: 'sass-loader',
-        options: { sourceMap: true }
-      }
-    ],
-    fallback: 'style-loader'
-  })
-};
+    },
+  ],
+});
+const copySite = new CopyPlugin({ patterns: [{ from: './example', to: './' }] });
 
-const devLanguage = require('./languages/en.json');
+const extractSass = new ExtractCSSPlugin({ filename: 'leaflet-measure.css' });
+
+const locales = { en: './languages/en.json' };
 
 module.exports = {
+  mode: 'development',
   entry: ['./src/leaflet-measure.js'],
   output: {
-    filename: 'leaflet-measure.js',
-    path: BUILD_DIR
+    filename: 'leaflet-measure.[locale].js',
+    path: BUILD_DIR,
   },
   devServer: {
-    contentBase: BUILD_DIR
+    static: BUILD_DIR,
   },
   module: {
-    rules: [htmlLoader, scssLoader]
+    rules: [
+      {
+        test: /\.html$/,
+        use: { loader: 'html-loader', options: { interpolate: true } },
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          ExtractCSSPlugin.loader,
+          { loader: 'css-loader' },
+          { loader: 'resolve-url-loader' },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
   },
-  plugins: [copySite, copyAssets, extractSass, new I18nPlugin(devLanguage)],
-  devtool: 'eval-source-map'
+  plugins: [copySite, copyAssets, extractSass, new LocalizePlugin({ locales })],
+  devtool: 'eval-source-map',
 };
